25e6bdf17937c5f963bd1939c5bea7c8db8e5f4c
sew-gem <theuflower0112@gmail.com>
2023-08-07 15:32:04 +0700

    new commit

--- src/Java2048.java
+++ src/Java2048.java
@@ -1,4 +1,4 @@
-public class Java2048{
+ public class Java2048{
     public static void main(String[] args){
         Game g = new Game(); //intialized the gameBoard with [4][4]
         g.printArray();
